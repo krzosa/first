ea5842a454dfba78b83c51824252fb65b32e8088
Krzosa Karol <krzosa.karol@gmail.com>
2024-01-05 14:15:13 +0100

    Small script cleanup

--- build.sh
+++ build.sh
@@ -1,18 +1,3 @@
 #!/usr/bin/env bash
-mkdir build
-cd build
-set -e
-g++ -o bld ../bld_main.cpp -g
-cd ..
-./build/bld
-# gcc -o test_filesystem ../test/test_filesystem.c -Wno-write-strings
-# ./test_filesystem
-# gcc -o test_array ../test/test_array.cpp -fno-exceptions -fno-rtti -Wno-write-strings
-# ./test_array
-# gcc -o test_table ../test/test_table.cpp -fno-exceptions -fno-rtti -Wno-write-strings
-# ./test_table
-# gcc -o compile_as_c ../test/main.c
-# ./compile_as_c
-
-#-Wno-writable-strings
-cd ..
+gcc -o bld bld_main.cpp -g
+./bld
